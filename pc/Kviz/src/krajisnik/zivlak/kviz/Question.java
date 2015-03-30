283fc45b7d0f5c4b8cd64fdbe9cfca609b7d5488
zivlakmilos <zivlakmilos@gmail.com>
2015-03-30 22:41:46 +0200

    Android Kviz

--- pc/Kviz/src/krajisnik/zivlak/kviz/Question.java
+++ pc/Kviz/src/krajisnik/zivlak/kviz/Question.java
@@ -12,4 +12,8 @@ public class Question
     private int id;
     private int type;
     private int typeId;
+    private String question;
+    private String answer;
+    private String a, b, c, d, e, f, g;
+    private String a1, b1, v1, d1, e1, f1, g1;
 }
