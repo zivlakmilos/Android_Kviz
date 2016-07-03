7fb49aa0542ea5bbb79104f7369c132f950a1b76
Milos Zivlak <zivlakmilos@gmail.com>
2016-07-03 17:50:34 +0200

    Obrisan suvisan kod, koriscen za testiranje

--- Service/src/data/MAsocijacije.cpp
+++ Service/src/data/MAsocijacije.cpp
@@ -29,9 +29,6 @@ QVariant MAsocijacije::data(const QModelIndex &index, int role) const
         case Qt::DisplayRole: case Qt::EditRole:
             return podatakZaPrikaz(index.column(), index.row());
             break;
-        case Qt::ToolTipRole:
-            return QString("test");
-            break;
     }
     
     return QVariant();
